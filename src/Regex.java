09fabee5e289046a361dc1a176a4d8562421fe0c
Rifat Hossain <hossainrifat48@outlook.com>
2021-08-23 13:05:34 +0600

    Seperated Each element of a given String

--- src/Regex.java
+++ src/Regex.java
@@ -8,21 +8,6 @@ public class Regex {
         Scanner input_str = new Scanner(System.in);
         System.out.println("Enter String which needs to contain '(', ')', '*', '|', '+'");
         String user_str = input_str.nextLine();
-
-
-        //contain all the methods in a stack the pop the least essential ones
-        Stack<Character> stack_str = new Stack<>();
-
-        for(int i =0 ; i < user_str.length(); i ++){
-            for(int j = i ; j <= i; j ++){
-               stack_str.push(user_str.charAt(j));
-//                System.out.println(user_str.charAt(j));
-            }
-        }
-
-        System.out.println(stack_str);
-
-
     }
 
 }
