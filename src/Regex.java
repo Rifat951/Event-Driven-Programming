d15c76b784cf35fd66c5f237f20a356c711f59c0
Rifat951 <Loop1080>
2021-08-23 12:09:20 +0600

    Seperated Each element of a given String

--- src/Regex.java
+++ src/Regex.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import  java.util.Stack;
 
 public class Regex {
 
@@ -6,10 +7,21 @@ public class Regex {
         //Input the string from user's end
         Scanner input_str = new Scanner(System.in);
         System.out.println("Enter String which needs to contain '(', ')', '*', '|', '+'");
-
         String user_str = input_str.nextLine();
 
-        System.out.println(user_str);
+
+        //contain all the methods in a stack the pop the least essential ones
+        Stack<Character> stack_str = new Stack<>();
+
+        for(int i =0 ; i < user_str.length(); i ++){
+            for(int j = i ; j <= i; j ++){
+               stack_str.push(user_str.charAt(j));
+//                System.out.println(user_str.charAt(j));
+            }
+        }
+
+        System.out.println(stack_str);
+
 
     }
 
