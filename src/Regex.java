35e44df7490b39e1cb79bbc7b5939a110311a392
Rifat Hossain <hossainrifat48@outlook.com>
2021-08-24 13:53:22 +0600

    poping parenthesis

--- src/Regex.java
+++ src/Regex.java
@@ -1,29 +1,30 @@
+
+import java.util.Arrays;
 import java.util.Scanner;
-import  java.util.Stack;
 
 public class Regex {
 
-    public static void main(String[] args){
-        //Input the string from user's end
-        Scanner input_str = new Scanner(System.in);
-        System.out.println("Enter String which needs to contain '(', ')', '*', '|', '+'");
-        String user_str = input_str.nextLine();
+    public static void main(String[] args) {
 
-        //contain all the methods in a stack the pop the parenthesis
-        Stack<Character> stack_str = new Stack<>();
-
-        for(int i =0 ; i < user_str.length(); i ++){
-            for(int j = i ; j <= i; j ++){
-                stack_str.push(user_str.charAt(j));
-                if(stack_str.contains('(') || stack_str.contains(')')){
-                    stack_str.pop();
-                }
-//                System.out.println(user_str.charAt(j));
-            }
+        // need to input three line of strings
+        Scanner input = new Scanner(System.in);
+        System.out.println("Number of lines we want to display (int) : ");
+        // splite line
+        String[] str = new String [input.nextInt()];
+        // input string
+        input.nextLine();
+        for (int index = 0; index < str.length; index++)
+        {
+            str[index] = input.nextLine();
         }
+//        System.out.println(Arrays.toString(str));
 
-        // just to check if the output are working or not
-        System.out.println(stack_str);
+        System.out.println("\nResults: ");
+        //for-each loop to print the string
+        for(String strIndex: str)
+        {
+            System.out.println(strIndex);
+        }
     }
 
 }
