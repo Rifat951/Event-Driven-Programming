c3628911ca4c7c2ac983ac5572d1c9e6888b2601
Rifat Hossain <31892050+Rifat951@users.noreply.github.com>
2021-08-24 13:10:34 +0600

    Update Regex.java

--- src/Regex.java
+++ src/Regex.java
@@ -8,22 +8,11 @@ public class Regex {
         Scanner input_str = new Scanner(System.in);
         System.out.println("Enter String which needs to contain '(', ')', '*', '|', '+'");
         String user_str = input_str.nextLine();
-
-        //contain all the methods in a stack the pop the parenthesis
-        Stack<Character> stack_str = new Stack<>();
-
-        for(int i =0 ; i < user_str.length(); i ++){
-            for(int j = i ; j <= i; j ++){
-                stack_str.push(user_str.charAt(j));
-                if(stack_str.contains('(') || stack_str.contains(')')){
-                    stack_str.pop();
-                }
-//                System.out.println(user_str.charAt(j));
-            }
-        }
-
-        // just to check if the output are working or not
-        System.out.println(stack_str);
+        
+        // 1. transform the expression to tree(usage of dfs maybe) ?
+        // 2. tree to transition table(multi array) ?
+        // 3 display the transition table
+        //
     }
 
 }
