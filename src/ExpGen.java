a924da72e4616b5304c9034cb4b202f0047f6153
Rifat Hossain <hossainrifat48@outlook.com>
2021-08-26 20:25:13 +0600

    ConvertInator is ready.. still needs for test

--- src/ExpGen.java
+++ src/ExpGen.java
@@ -4,6 +4,8 @@ public class ExpGen {
 
 
     private static Stack<Character> speicalChars = new Stack<Character>();
+    private static Stack<NFA> StackedValofNfa = new Stack<NFA>();
+    private static  int IdStates = 0;
 
     private  static void SpeicalCharOperation{
 
@@ -27,7 +29,64 @@ public class ExpGen {
         }
     }
 
+    // RegExpression string value will be parsed here
+
+    public static NFA NfaConveter(String RegExpression){
+
+            // RegExpression need to have a function which can concat between symbols eg. ()() like this
+
+
+    }
+
+
+
+    public static boolean isChar(char inputchar){
+        if(inputchar == 'a' || inputchar == 'b' || inputchar == 'c' || inputchar == 'e' ) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public static String ConcatInator(String RegExpression){
+
+        String str = new String("");
+
+        for(int indexofString = 0 ; indexofString < RegExpression.length()-1 ; indexofString++){
+            //need a boolean beacause we might need to input some chars at the same time
+            char firsttempval = RegExpression.charAt(indexofString);
+            char secondtempval = RegExpression.charAt(indexofString+1);
+                if( (isChar(firsttempval) && isChar(secondtempval)) || (isChar(firsttempval) && secondtempval == '(') ||
+                        (firsttempval == ')' && isChar(secondtempval)) || (firsttempval == '*' && isChar(secondtempval)) ||
+                        (firsttempval == '*' && secondtempval == '(') || (firsttempval == ')' && secondtempval == '(')){
+
+                    str = str + firsttempval + ".";
+
+                }
+                else {
+                    str = str + firsttempval;
+                }
+        }
+
+        str =  str + RegExpression.charAt(RegExpression.length() -1 );
+        return str;
+    }
+
+
+
     public static void merge(){
+        NFA firstCharofNfa = StackedValofNfa.pop();
+        NFA secondCharofNfa = StackedValofNfa.pop();
+
+
+        TransState startstate = new TransState (IdStates++);
+        TransState endstate = new TransState (IdStates++);
+
+        // Set transition table begin and end of each SubNfa
+        startstate.Transition(endstate, 'e');
+        // well we nee to get NFA, otherwise cannot progress.
+
     }
 
     public  static void repeatation(){
