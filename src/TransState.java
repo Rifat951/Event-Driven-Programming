e1af47fca0f4ea705aecca974ecbbd65369e463f
Rifat Hossain <hossainrifat48@outlook.com>
2021-08-26 15:37:53 +0600

    DFA Implementation

--- src/TransState.java
+++ src/TransState.java
@@ -11,7 +11,10 @@ public class TransState {
 
         //transition states for DFA
     private Set<TransState> statesdfa;
+    private Map<Character,ArrayList<TransState>> nextState;
     int IdOfStates;
+    boolean accpetState;
+
     public TransState(Set<TransState> statesdfa, int ID){
         this.setStates(statesdfa);
         this.setIdOfStates(IdOfStates);
@@ -23,8 +26,19 @@ public class TransState {
         this.statesdfa = statesdfa;
     }
 
+    public int getIdOfStates(){
+        return IdOfStates;
+    }
+
     public void setIdOfStates(int IdOfStates){
         this.IdOfStates = IdOfStates;
+    }
+    public void setNextState(HashMap<Character, ArrayList<TransState>> newmap){
+        this.nextState = newmap;
+    }
+
+    public Map<Character, ArrayList<TransState>> getNextState(){
+        return nextState;
     }
 
 }
