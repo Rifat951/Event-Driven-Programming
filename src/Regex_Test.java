18f462fc00c66682cc31a3b6b828870d8b29754d
Rifat Hossain <hossainrifat48@outlook.com>
2021-09-02 01:05:40 +0600

    compile testclass

---
+++ src/Regex_Test.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+import static org.junit.Assert.*;
+
+public class Regex_Test {
+
+    @Test
+    public void main() throws Exception {
+
+    }
+}
---
+++ src/testpck/RegexEngineTest.java
@@ -0,0 +1,11 @@
+package testpck;
+
+import static org.junit.Assert.*;
+
+public class RegexEngineTest {
+
+    public void main() {
+        String str = "rafi";
+        assertEquals("tofi", str);
+    }
+}
